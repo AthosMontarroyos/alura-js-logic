d876e8ece07b82717745670895a2ca0d06273f60
Athos Montarroyos <athosg05@gmail.com>
2024-08-26 15:48:31 -0300

    terminando o inicio

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+alert('Boas Vindas ao jogo')
+var numero = 22
+let chute = prompt('escolha um nuemro de 1 e 30')
+
+if (numero==chute) {
+   console.log('Voce acertou o numero secreto')
+} else {
+    console.log(`Você errou o Numero secreto era ${numero}`)
+}
