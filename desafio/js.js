63a22207ca6edce7297501a42b5c671f0645287a
Athos Montarroyos <athosg05@gmail.com>
2024-08-26 16:14:59 -0300

    coemçando o desafio

---
+++ desafio/js.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao site')
+var nome = prompt("Qual seu nome?")
+var idade = prompt("Qual sua idadade?")
+var numerodevendas = 1000
+var saldoDisponivel = 50
+var nada = ''
+if (nome != nada && idade > 0 ){
+    Text.innerHTML
+} else {
+    window.alert('ERROR')
+}
