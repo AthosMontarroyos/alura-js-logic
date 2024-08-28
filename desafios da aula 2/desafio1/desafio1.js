80010634c3767269fcf57392eac4d224fbd3a5d1
Athos Montarroyos <athosg05@gmail.com>
2024-08-28 15:03:22 -0300

    terminando o exercio 1,2,5

---
+++ desafios da aula 2/desafio1/desafio1.js
@@ -0,0 +1,6 @@
+var dia = (window.prompt('Que dia é hoje?'))
+if (dia == 1 || dia == 7 ) {
+    window.alert('Bom Fim de Semana!')
+} else {
+    window.alert('Boa Semana')
+}
---
+++ desafios da aula 2/desafio2/desafio2.js
@@ -0,0 +1,10 @@
+var numero = window.prompt('digite um numero.')
+if (numero > 0) {
+    window.alert('este numero é positivo')
+} else if (numero < 0) {
+ window.alert('Este numero é negativo')
+} else if (numero == 0){
+    window.alert('Este numero é 0')
+} else {
+    window.alert('Isto não e um numero')
+}
