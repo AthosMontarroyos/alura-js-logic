def9f6f5ebd329d0139cca9a1e95956f1c0a24d2
Athos Montarroyos <athosg05@gmail.com>
2024-08-28 16:01:33 -0300

    Terminando os videos da parte 3 e começando os desafios

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,9 @@
 alert('Boas Vindas ao jogo')
 var numero = 22
-let chute = prompt('escolha um nuemro de 1 e 30')
-
+let chute = ''
+var tentivas = ''
+while (chute != numero){
+    chute = prompt('escolha um nuemro de 1 e 30')
 if (numero == chute) {
    alert('Voce acertou o numero secreto')
 } else {
@@ -10,4 +12,5 @@ if (numero == chute) {
     }else if (chute < numero){
         alert(`Você ERROU mas ${chute} é MENOR que o numero certo`)
     }
-}
+} tentivas ++
+} alert(`vc teve ${tentivas} tentativas`)
