f9e2f9afd23ce4fcff7b69f46a9518bd5a1ff578
Athos Montarroyos <athosg05@gmail.com>
2024-08-28 14:34:03 -0300

    começando o desafio 2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,7 +3,7 @@ var numero = 22
 let chute = prompt('escolha um nuemro de 1 e 30')
 
 if (numero == chute) {
-   console.log('Voce acertou o numero secreto')
+   alert('Voce acertou o numero secreto')
 } else {
-    console.log(`Você errou o Numero secreto era ${numero}`)
+    alert(`Você errou :( , o Numero secreto era ${numero}`)
 }
